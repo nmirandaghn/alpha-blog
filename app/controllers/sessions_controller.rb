699e39d3ac7ee8a2213094714a557bed53d9f098
Nelson Miranda <nmirandaghn@gmail.com>
2018-02-09 19:13:43 -0600

    Login form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = 'You have succesfully logged in'
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Access denied'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'delete', to: 'sessions#destroy'
 end
